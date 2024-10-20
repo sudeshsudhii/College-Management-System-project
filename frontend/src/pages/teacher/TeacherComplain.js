4d1e6380de0a1e60f54cfdae5f2a49f87b80cc0e
sudesh sudhii <sudeshtiger1999@gmail.com>
2024-10-20 15:30:00 +0530

    teacher complain page added

---
+++ frontend/src/pages/teacher/TeacherComplain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeacherComplain = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100vh' }}>
+      <div>TeacherComplain</div>
+    </div>
+  );
+}
+
+export default TeacherComplain;
